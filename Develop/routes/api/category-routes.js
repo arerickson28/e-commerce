2c8624d4d87a1ba6f2ab04322ebcbb437c8f42b2
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-08-09 14:36:23 -0500

    get routes work for Category, Product, and Tag

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,26 +7,32 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try { 
-    const categoryData = await Category.findAll({
-      include: [{ model: Product }] 
-    });
+    const categoryData = await Category.findAll();
     res.status(200).json(categoryData)
   } catch(err) {
     res.status(400).json(err)
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  Category.findOne({ where: {id: req.params.id}, 
-    include: [Product],
-
-  }).then((category) => req.json(category)).catch((err) => res.status(400).json(err))
+  try {
+    const categoryOne = await Category.findByPk(req.params.id)
+    res.status(200).json(categoryOne);
+  } catch(err) {
+    res.status(400).json(err)
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory)
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
